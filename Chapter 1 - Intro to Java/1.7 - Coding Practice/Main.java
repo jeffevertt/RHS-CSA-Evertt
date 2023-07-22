d42ba7075f27decf4ca23f3f69a3bdc02a9db328
jeffevertt <jevertt@lwsd.org>
2023-07-22 12:56:52 -0700

    added 1.7 "coding practice"

---
+++ Chapter 1 - Intro to Java/1.7 - Coding Practice/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        // You should add new functions to this class, but
+        //  do not modify this function (main).
+        System.out.println("--- Exercise 5 ---");
+        exercise5();
+        System.out.println("\n--- Exercise 6 ---");
+        exercise6();
+        System.out.println("\n--- Exercise 7 ---");
+        exercise7();
+        System.out.println("\n--- Exercise 8 ---");
+        exercise8();
+        System.out.println("\n--- Exercise 9 ---");
+        exercise9();
+        System.out.println("\n--- Exercise 10 ---");
+        exercise10();
+    }
+
+    public static void exercise5() {
+        // TODO: Your code for this exercise goes here
+    }
+
+    public static void exercise6() {
+        // TODO: Your code for this exercise goes here
+    }
+
+    public static void exercise7() {
+        // TODO: Your code for this exercise goes here
+    }
+
+    public static void exercise8() {
+        // TODO: Your code for this exercise goes here
+    }
+
+    public static void exercise9() {
+        // TODO: Your code for this exercise goes here
+    }
+
+    public static void exercise10() {
+        // TODO: Your code for this exercise goes here
+    }
+}
