3f7846e05912d2ba9566d07c7630222248b79c78
jeffevertt <jevertt@lwsd.org>
2023-09-16 09:22:51 -0700

    added a minor comment

--- Chapter 7 - Arrays/7.2 - Using Arrays/AppMain.java
+++ Chapter 7 - Arrays/7.2 - Using Arrays/AppMain.java
@@ -1,5 +1,6 @@
 public class AppMain {
     public static void main(String[] args) {
+        // Do not modify the code in this method (edit the methods below main)
         System.out.println("--------- arrayToString ---------");
         System.out.println(arrayToString(new int[] { 1, 2, 3, 4 }));
         System.out.println(arrayToString(new int[] { -1, 5, 0 }));
