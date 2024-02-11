5714b637d0366c93a6f9fce7912df609fab766cd
jeffevertt <jevertt@lwsd.org>
2024-02-11 11:20:16 -0800

    (zelevator) little clean up

--- Chapter 13 - Searching & Sorting/zelevators/logic/OtherElevatorController.java
+++ Chapter 13 - Searching & Sorting/zelevators/logic/OtherElevatorController.java
@@ -12,24 +12,13 @@ public class OtherElevatorController implements ElevatorController {
     // PASTE Other Person's code HERE (BELOW THIS)
 
     //  (replace these placeholder methods)
-    // public String getStudentName() { return null; }
-    // public int getStudentPeriod() { return -1; }
-    // public void onGameStarted(Game game) { }
-    // public void onElevatorRequestChanged(int floorIdx, Direction dir, boolean reqEnable) { }
-    // public void onFloorRequestChanged(int elevatorIdx, int floorIdx, boolean reqEnable) { }
-    // public void onElevatorArrivedAtFloor(int elevatorIdx, int floorIdx, Direction travelDirection) { }
-    // public void onUpdate(double deltaTime) { }
-
-    // PASTE Other Person's code HERE (ABOVE THIS)
-
-
-    // TEST CODE
-    public String getStudentName() { return "Other Player"; }
-    public int getStudentPeriod() { return 3; }
+    public String getStudentName() { return "<NONE>"; }
+    public int getStudentPeriod() { return -1; }
     public void onGameStarted(Game game) { }
     public void onElevatorRequestChanged(int floorIdx, Direction dir, boolean reqEnable) { }
     public void onFloorRequestChanged(int elevatorIdx, int floorIdx, boolean reqEnable) { }
     public void onElevatorArrivedAtFloor(int elevatorIdx, int floorIdx, Direction travelDirection) { }
     public void onUpdate(double deltaTime) { }
-    // TEST CODE
+
+    // PASTE Other Person's code HERE (ABOVE THIS)
 }
