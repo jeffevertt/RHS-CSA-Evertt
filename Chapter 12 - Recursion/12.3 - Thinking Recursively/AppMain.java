ebf235d9a4d364b8f3c2072e79d6f92a336d6a51
jeffevertt <jevertt@lwsd.org>
2024-03-11 16:41:51 -0700

    (12.3 thinking recurively) lab first time commit

---
+++ Chapter 12 - Recursion/12.3 - Thinking Recursively/AppMain.java
@@ -0,0 +1,17 @@
+public class AppMain {
+    public static void main(String[] args) {
+        Practice practice = new Practice();
+
+        System.out.println("-------- isPalindrome --------");
+        // TODO: add your test cases here
+
+        System.out.println("\n-------- sumDigits --------");
+        // TODO: add your test cases here
+
+        System.out.println("\n-------- createString --------");
+        // TODO: add your test cases here
+        
+        System.out.println("\n-------- minValue --------");
+        // TODO: add your test cases here
+    }
+}
