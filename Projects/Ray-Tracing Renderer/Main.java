ff9b2c0be0ffd13cffa79cf7ade955d466d04414
jeffevertt <jevertt@lwsd.org>
2024-04-21 10:27:55 -0700

    typo

--- Projects/Ray-Tracing Renderer/Main.java
+++ Projects/Ray-Tracing Renderer/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // Create up the app, it kicks off the whole process...
         if (!App.createApp(new Renderer())) {
-            throw new Error("Could not create the game :(");
+            throw new Error("Could not create the app :(");
         }
     }
 }
