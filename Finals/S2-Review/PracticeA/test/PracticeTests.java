afd1062d484885052814d9c7b4b05521ebfeb9dc
jeffevertt <jevertt@lwsd.org>
2024-06-05 16:33:41 -0700

    swapped test param order

--- Finals/S2-Review/PracticeA/test/PracticeTests.java
+++ Finals/S2-Review/PracticeA/test/PracticeTests.java
@@ -7,41 +7,41 @@ public class PracticeTests {
     @Test
     public void testStrPatternA() {
         Practice p = new Practice();
-        assertEquals(p.strPatternA(3, 2), "--\n-----\n--------");
-        assertEquals(p.strPatternA(2, 5), "-----\n-------");
-        assertEquals(p.strPatternA(1, 3), "---");
-        assertEquals(p.strPatternA(2, 3), "---\n-----");
+        assertEquals("--\n-----\n--------", p.strPatternA(3, 2));
+        assertEquals("-----\n-------", p.strPatternA(2, 5));
+        assertEquals("---", p.strPatternA(1, 3));
+        assertEquals("---\n-----", p.strPatternA(2, 3));
     }
 
     @Test
     public void testStrPatternB() {
         Practice p = new Practice();
-        assertEquals(p.strPatternB("abc", 3), "abc, abc, abc");
-        assertEquals(p.strPatternB("x", 4), "x, x, x, x");
-        assertEquals(p.strPatternB("ttfn", 1), "ttfn");
-        assertEquals(p.strPatternB("", 5), "");
+        assertEquals("abc, abc, abc", p.strPatternB("abc", 3));
+        assertEquals("x, x, x, x", p.strPatternB("x", 4));
+        assertEquals("ttfn", p.strPatternB("ttfn", 1));
+        assertEquals("", p.strPatternB("", 5));
     }
 
     @Test
     public void testPercToLetterGrade() {
         Practice p = new Practice();
-        assertEquals(p.percToLetterGrade(95), "A");
-        assertEquals(p.percToLetterGrade(89.9), "B");
-        assertEquals(p.percToLetterGrade(70), "C");
-        assertEquals(p.percToLetterGrade(61.8), "D");
-        assertEquals(p.percToLetterGrade(20), "N");
+        assertEquals("A", p.percToLetterGrade(95));
+        assertEquals("B", p.percToLetterGrade(89.9));
+        assertEquals("C", p.percToLetterGrade(70));
+        assertEquals("D", p.percToLetterGrade(61.8));
+        assertEquals("N", p.percToLetterGrade(20));
     }
 
     @Test
     public void testStrList() {
         Practice p = new Practice();
         int[] resultA = { 1, 2, 3, 4 };
-        Assert.assertArrayEquals(p.strToList("1,2,3,4", ','), resultA);
+        Assert.assertArrayEquals(resultA, p.strToList("1,2,3,4", ','));
         int[] resultB = { 5, 2 };
-        Assert.assertArrayEquals(p.strToList("5; 2", ';'), resultB);
-        Assert.assertArrayEquals(p.strToList("5; ;  2", ';'), resultB);
+        Assert.assertArrayEquals(resultB, p.strToList("5; 2", ';'));
+        Assert.assertArrayEquals(resultB, p.strToList("5; ;  2", ';'));
         int[] resultC = { 12, 51 };
-        Assert.assertArrayEquals(p.strToList("12* 51", '*'), resultC);
+        Assert.assertArrayEquals(resultC, p.strToList("12* 51", '*'));
         assertNull(p.strToList("5; 2", ','));
         assertNull(p.strToList("5; 1 2", ';'));
     }
--- Finals/S2-Review/PracticeB/test/PracticeTests.java
+++ Finals/S2-Review/PracticeB/test/PracticeTests.java
@@ -9,32 +9,32 @@ public class PracticeTests {
     public void strCatColumn() {
         Practice p = new Practice();
         String[][] m1 = { { "a", "b", "c" }, { "d", "e", "f" } };
-        assertEquals(p.strCatColumn(m1, 0), "ad");
-        assertEquals(p.strCatColumn(m1, 1), "be");
+        assertEquals("ad", p.strCatColumn(m1, 0));
+        assertEquals("be", p.strCatColumn(m1, 1));
         assertNull(p.strCatColumn(m1, 3));
         assertNull(p.strCatColumn(m1, -1));
         String[][] m2 = { { "sbd", "axz" }, { "yy", "vhs" }, { " t", "shs" }, { "**", "854" } };
-        assertEquals(p.strCatColumn(m2, 0), "sbdyy t**");
-        assertEquals(p.strCatColumn(m2, 1), "axzvhsshs854");
+        assertEquals("sbdyy t**", p.strCatColumn(m2, 0));
+        assertEquals("axzvhsshs854", p.strCatColumn(m2, 1));
     }
 
     @Test
     public void calcSeriesRecursive() {
         Practice p = new Practice();
-        assertEquals(p.calcSeriesRecursive(2,1,3), 15);
-        assertEquals(p.calcSeriesRecursive(-3,10,12), -114);
-        assertEquals(p.calcSeriesRecursive(-3,10,0), 0);
+        assertEquals(15, p.calcSeriesRecursive(2,1,3));
+        assertEquals(-114, p.calcSeriesRecursive(-3,10,12));
+        assertEquals(0, p.calcSeriesRecursive(-3,10,0));
     }
 
     @Test
     public void createFibonacciArray() {
         Practice p = new Practice();
         int[] fibA = { 1, 1, 2, 3, 5 };
-        assertArrayEquals(p.createFibonacciArray(1, 5), fibA);
+        assertArrayEquals(fibA, p.createFibonacciArray(1, 5));
         int[] fibB = { 8, 13, 21, 34 };
-        assertArrayEquals(p.createFibonacciArray(8, 4), fibB);
+        assertArrayEquals(fibB, p.createFibonacciArray(8, 4));
         assertNull(p.createFibonacciArray(4, 3));
         int[] fibC = { 233, 377 };
-        assertArrayEquals(p.createFibonacciArray(233, 2), fibC);
+        assertArrayEquals(fibC, p.createFibonacciArray(233, 2));
     }
 }
